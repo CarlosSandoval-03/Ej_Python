e2c38f38aefbab5c13416016fcd48a88aca59bf8
Carlos Sandoval <csandovalc@unal.edu.co>
2021-05-18 18:32:54 -0500

    Base file creation

---
+++ Ej43a49.py
@@ -0,0 +1,11 @@
+# Un polinomio de grado n, como P(x) = anxn + an−1xn−1 + ⋯ + a1x1 + a0x0 se puede representar mediante un arreglo de reales de la siguiente manera: (a0,a1,...,an−1,an).
+
+# Usando esta representacion hacer un programa que le permita al usuario leer dos polinomios y escoger mediante un menu, una de las siguientes operaciones sobre dichos polinomios:
+# 43. Evaluar: Lee un real e imprime la evaluacion de los dos polinomios en dicho dato.
+# 44. Sumar: Calcula el polinomio suma y lo imprime.
+# 45. Resta: Calcula el polinomio resta y lo imprime.
+# 46. Multiplicar: Calcula el polinomio multiplicacion y lo imprime.
+# 47. Dividir: Calcula el polinomio division del primer polinomio por el segundo y lo imprime.
+# 48. Residuo: Calcula el polinomio residuo de la division del primero por el segundo y lo imprime.
+# 49. Salir: Permite salir de la aplicacion al usuario.
+# Despues de realizada la operacion el menu se debe presentar de nuevo hasta que el usuario desee salir.
