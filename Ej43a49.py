2324c03d43abf1a15c11eed8ba18d02be34ac523
Carlos Sandoval <csandovalc@unal.edu.co>
2021-05-24 20:27:46 -0500

    Methods "index" and "split" are programmed

--- Ej43a49.py
+++ Ej43a49.py
@@ -1,7 +1,38 @@
-from Ej30 import nuevoMeterNumeros
-# Un polinomio de grado n, como P(x) = anxn + an−1xn−1 + ⋯ + a1x1 + a0x0 se puede representar mediante un arreglo de reales de la siguiente manera: (a0,a1,...,an−1,an).
+from typing import Counter
+from Ej35a42 import limpiarConsola
 
+
+def metodoIndex(element, seeker):
+    count = 0
+    for parts in element:
+        if parts == seeker:
+            return count
+        else:
+            count += 1
+    return -1
+
+
+def metodoSplit(string: str, separador=" ") -> list:
+    listOfTerms = []
+    temp = ""
+    for element in string:
+        if element != separador:
+            temp += element
+        else:
+            listOfTerms.append(temp)
+            temp = ""
+    if len(temp) > 0:
+        listOfTerms.append(temp)
+    return listOfTerms
+
+# Un polinomio de grado n, como P(x) = anxn + an−1xn−1 + ⋯ + a1x1 + a0x0 se puede representar mediante un arreglo de reales de la siguiente manera: (a0,a1,...,an−1,an).
 # Usando esta representacion hacer un programa que le permita al usuario leer dos polinomios y escoger mediante un menu, una de las siguientes operaciones sobre dichos polinomios:
+
+
+def meterPolinomios() -> list:
+    limpiarConsola()
+
+
 # 43. Evaluar: Lee un real e imprime la evaluacion de los dos polinomios en dicho dato.
 # 44. Sumar: Calcula el polinomio suma y lo imprime.
 # 45. Resta: Calcula el polinomio resta y lo imprime.
@@ -12,12 +43,8 @@ from Ej30 import nuevoMeterNumeros
 # Despues de realizada la operacion el menu se debe presentar de nuevo hasta que el usuario desee salir.
 
 
-def meterPolinomios():
-    a = None
-
-
 def main():
-    print("Hola")
+    pass
 
 
 if __name__ == "__main__":
